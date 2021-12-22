b54c8a51500fc5ecfd871b8718165b5ba2763384
Nada <nadaelgendy@Nadas-MacBook-Air.local>
2021-12-22 18:06:07 +0200

    fetch data from api and updating ui

--- server.js
+++ server.js
@@ -2,37 +2,44 @@
 projectData = {};
 
 // Require Express to run server and routes
-const express = require('express');
+const express = require("express");
 // Start up an instance of app
 const app = express();
 
 // Require Cors to run server and routes
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // Require body parser to run server and routes
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser());
 
 // Add port
 const PORT = 1111;
-app.listen(PORT, function (){
-    console.log(`server running on port ${PORT}`);
+app.listen(PORT, function() {
+  console.log(`server running on port ${PORT}`);
 });
-
-
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-
-// Cors for cross origin allowance
-
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+
+// Callback function to complete GET '/all'
+const getAll = (req, res) => res.status(200).send(projectData);
+// Get route
+app.get("/all", getAll);
+
+// Callback function to complete POST '/add'
+const postData = (req, res) => {
+  projectData = req.body;
+  console.log(projectData);
+  res.status(200).send(projectData);
+};
+// Post route
+app.post("/add", postData);
--- website/app.js
+++ website/app.js
@@ -2,16 +2,109 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + "." + d.getDate() + "." + d.getFullYear();
 
+// The Api URL
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
 
+// API Key for OpenWeatherMap API with celsius
+const apiKey = ",&appid=8be7026531257334b3370c1db3daeecb&units=metric";
 
+// the URL of the server to post data
+const server = "http://localhost:1111";
 
+// const getDate = async function () {
+//     const request = await fetch('')
+//     try{
 
+//     } catch(err){
+//         console.log(err);
+//     }
+// }
 
+// generateData //
 
+const generateData = () => {
+  //get value after click on the button
+  const zip = document.getElementById("zip").value;
+  const feelings = document.getElementById("feelings").value;
 
-/*
-api.openweathermap.org/data/2.5/weather?zip={zip code},{&appid={8be7026531257334b3370c1db3daeecb&metric}
+  // getWeatherData return promise
+  getWeatherData(zip).then(data => {
+    //making sure from the received data to execute rest of the steps
+    if (data) {
+      const {
+        main: { temp },
+        name: city,
+        weather: [{ description }]
+      } = data;
 
-*/
+      const info = {
+        newDate,
+        temp: Math.round(temp), // to get integer number
+        feelings
+      };
+
+      postData(server + "/add", info);
+
+      updatingUI();
+      //   document.getElementById('entry').style.opacity = 1;
+    }
+  });
+};
+
+// Function called by event listener
+document.getElementById("generate").addEventListener("click", generateData);
+
+//Function to GET Web API Data
+const getWeatherData = async zip => {
+  try {
+    const res = await fetch(baseURL + zip + apiKey);
+    const data = await res.json();
+
+    if (data.cod != 200) {
+      // display the error message on UI
+      error.innerHTML = data.message;
+      setTimeout(_ => (error.innerHTML = ""), 2000);
+      throw `${data.message}`;
+    }
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Function to POST data
+const postData = async (url = "", info = {}) => {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(info)
+  });
+
+  try {
+    const newData = await res.json();
+    console.log(`You just saved`, newData);
+    return newData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Function to GET Project Data
+// and updating UI by this data
+const updatingUI = async () => {
+  const res = await fetch(server + "/all");
+  try {
+    const savedData = await res.json();
+
+    document.getElementById("date").innerHTML = savedData.newDate;
+    document.getElementById("temp").innerHTML = savedData.temp + "&degC";
+    document.getElementById("content").innerHTML = savedData.feelings;
+  } catch (error) {
+    console.log(error);
+  }
+};
